cd88e1f247f1a8798b8e3585e1c7067279132fdb
FrankiNarvaez <frannki03@gmail.com>
2024-12-24 17:47:59 -0500

    Create an account after create an user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  after_save :create_account
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_one :account
+
+  def create_account
+    Account.create(user_id: id)
+  end
 end
