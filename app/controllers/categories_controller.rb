a5719af11cd634a69d5098d83b296f9c72e1d83f
FrankiNarvaez <frannki03@gmail.com>
2024-12-24 20:05:38 -0500

    Solved error to before action at name update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ edit udpate destroy ]
+  before_action :set_category, only: %i[ edit update destroy ]
 
   def index
+    @categories = Category.all
   end
 
   def new
