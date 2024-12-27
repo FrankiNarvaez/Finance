bd25dfaf48e0856ecff970a0853f42080254104b
FrankiNarvaez <frannki03@gmail.com>
2024-12-27 11:14:39 -0500

    feature: filter by today

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,7 @@
 class AccountsController < ApplicationController
   def index
     account_id = current_user.account.id
-    @transactions = Transaction.account(account_id)
+    @transactions = Transaction.account(account_id).where(date: Date.today)
     @categories = Category.account(account_id)
     @banks = Bank.account(account_id)
   end
