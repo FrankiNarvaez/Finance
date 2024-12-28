7a5d3e42d4a8d1faedea0b5fbedae1b1f664be9c
FrankiNarvaez <frannki03@gmail.com>
2024-12-28 11:55:36 -0500

    fix: resolve redirect to delete

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -7,11 +7,23 @@ export default class extends Controller {
       this.floatTarget.classList.remove('scale-0')
       this.floatTarget.classList.add('scale-100')
     }, 50)
+
     this.element.addEventListener("turbo:submit-end", (event) => {
       if (event.detail.success) {
         Turbo.visit(event.detail.fetchResponse.response.url)
       }
     })
+
+    const handleTurboResponseDelete = (e) => {
+      if (e.detail.fetchResponse.response.statusText === "OK") {
+        document.removeEventListener("turbo:before-fetch-response", handleTurboResponseDelete);
+        setTimeout(() => {
+          Turbo.visit(e.detail.fetchResponse.response.url)
+        }, 100)
+      }
+    }
+
+    document.addEventListener("turbo:before-fetch-response", handleTurboResponseDelete)
   }
 
   close() {
