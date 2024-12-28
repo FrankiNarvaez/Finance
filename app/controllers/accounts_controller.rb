aa0de8b01e36b628b211d344c441cb3e948a9c7f
FrankiNarvaez <frannki03@gmail.com>
2024-12-27 19:40:18 -0500

    feature: information

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,5 +4,14 @@ class AccountsController < ApplicationController
     @transactions = Transaction.account(account_id).where(date: Date.today)
     @categories = Category.account(account_id)
     @banks = Bank.account(account_id)
+
+    @total_amount = 0
+    @transactions.each do |t|
+      if t.transaction_type == 0
+        @total_amount += t.amount
+      else
+        @total_amount -= t.amount
+      end
+    end
   end
 end
