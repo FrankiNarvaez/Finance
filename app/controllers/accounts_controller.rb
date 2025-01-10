0834b4f237d1b29c89dcfd12b8e2d60568d1b8df
FrankiNarvaez <frannki03@gmail.com>
2025-01-10 15:08:23 -0500

    modify: design

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,12 +1,13 @@
 class AccountsController < ApplicationController
   def index
     account_id = current_user.account.id
-    @transactions = Transaction.account(account_id).where(date: Date.today).load_async
+    @all_transactions = Transaction.account(account_id).load_async
     @categories = Category.with_attached_icon.account(account_id).load_async
     @banks = Bank.with_attached_picture.with_attached_icon.account(account_id).load_async
+    @today_transactions = Transaction.account(account_id).where(date: Date.today).load_async
 
     @total_amount_today = 0
-    @transactions.each do |t|
+    @today_transactions.each do |t|
       if t.transaction_type == 0
         @total_amount_today += t.amount
       else
@@ -17,6 +18,17 @@ class AccountsController < ApplicationController
     @total_balance = 0
     @banks.each do |b|
       @total_balance += b.balance
+    end
+
+    @total_income = 0
+    @total_expense = 0
+
+    @all_transactions.each do |t|
+      if t.transaction_type == 0
+        @total_income += t.amount
+      else
+        @total_expense += t.amount
+      end
     end
   end
 end
