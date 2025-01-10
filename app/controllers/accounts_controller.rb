a292ef8d9f2ddfde677bf82e9edf73c6bca6a637
FrankiNarvaez <frannki03@gmail.com>
2025-01-10 15:46:01 -0500

    feature: filter all transactions by month

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,10 @@
 class AccountsController < ApplicationController
   def index
     account_id = current_user.account.id
-    @all_transactions = Transaction.account(account_id).load_async
+    start_month = Date.new(Date.current.strftime("%Y").to_i, Date.current.strftime("%m").to_i, 1)
+    end_month = start_month.end_of_month
+    pp Date.current.strftime("%Y").to_i, Date.current.strftime("$m").to_i
+    @all_transactions = Transaction.account(account_id).where(date: start_month..end_month).load_async
     @categories = Category.with_attached_icon.account(account_id).load_async
     @banks = Bank.with_attached_picture.with_attached_icon.account(account_id).load_async
     @today_transactions = Transaction.account(account_id).where(date: Date.today).load_async
