fa27fb0b5633d9de63ce71f1f491498b7411cfb5
FrankiNarvaez <frannki03@gmail.com>
2025-01-11 21:31:45 -0500

    refactor

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,11 +3,11 @@ class AccountsController < ApplicationController
     account_id = current_user.account.id
     start_month = Date.new(Date.current.strftime("%Y").to_i, Date.current.strftime("%m").to_i, 1)
     end_month = start_month.end_of_month
-    pp Date.current.strftime("%Y").to_i, Date.current.strftime("$m").to_i
+
     @all_transactions = Transaction.account(account_id).where(date: start_month..end_month).load_async
     @categories = Category.with_attached_icon.account(account_id).load_async
     @banks = Bank.with_attached_picture.with_attached_icon.account(account_id).load_async
-    @today_transactions = Transaction.account(account_id).where(date: Date.today).load_async
+    @today_transactions = @all_transactions.select { |t| t.date == Date.today }
 
     @total_amount_today = 0
     @today_transactions.each do |t|
@@ -19,9 +19,7 @@ class AccountsController < ApplicationController
     end
 
     @total_balance = 0
-    @banks.each do |b|
-      @total_balance += b.balance
-    end
+    @banks.each { |b| @total_balance += b.balance }
 
     @total_income = 0
     @total_expense = 0
